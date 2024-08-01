044410f4b33d2e43d447ebc42e28c763049e2fb9
saksham-lussqvx <saksham.lussqvx.github@gmail.com>
2024-08-01 16:57:21 +0530

    Added base function for scraping details

--- google_maps_scraper.py
+++ google_maps_scraper.py
@@ -1,3 +1,9 @@
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
+
+def create_browser_session() -> None:
+    pass
+
+def scrape_all_details() -> None:
+    pass
