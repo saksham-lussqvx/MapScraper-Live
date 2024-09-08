b83762c35ce7752f9a9f362f45dee2ddea0cacaf
saksham-lussqvx <saksham.lussqvx.github@gmail.com>
2024-09-08 23:31:38 +0530

    Reduced all_ids_file

--- google_maps_scraper.py
+++ google_maps_scraper.py
@@ -20,7 +20,24 @@ import multiprocessing
 import flet as ft
 import os
 import json
-from all_css_ids import all_ids
+
+class all_ids:
+    def __init__(self):
+        pass
+    # create ids as class variables
+    name = "DUwDvf lfPIob"
+    rating = "F7nice"
+    review = "F7nice"
+    _type = "DkEaL"
+    address = "rogA2c"
+    timing = "ZDu9vd"
+    menu_dict = {"data-item-id": "menu"}
+    website_dict = {"data-item-id": "authority"}
+    location_plus_dict = {"data-tooltip": "Copy plus code"}
+    location_plus_code = "rogA2c"
+    phone_no_dict = {"data-tooltip": "Copy phone number"}
+    phone_no_code = "rogA2c"
+    reservation_dict = {"data-tooltip": "Open reservation link"}
 
 
 def check_settings_file() -> None:
